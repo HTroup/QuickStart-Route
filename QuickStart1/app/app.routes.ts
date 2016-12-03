6e2475d3d086017f29a6263c7a070e8e2cc1af4f
Henry Troup <htroup@acm.org>
2016-12-03 07:27:49 -0500

    app and dogs

---
+++ QuickStart1/app/app.routes.ts
@@ -0,0 +1,22 @@
+﻿// ====== ./app/app.routes.ts ======
+
+// from https://scotch.io/tutorials/routing-angular-2-single-page-apps-with-the-component-router
+
+// Imports
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CatListComponent } from './cats/cat-list.component';
+import { DogListComponent } from './dogs/dog-list.component';
+
+// Route Configuration
+export const routes: Routes = [
+    { path: 'cats', component: CatListComponent },
+    { path: 'dogs', component: DogListComponent }
+];
+
+// Deprecated provide
+// export const APP_ROUTER_PROVIDERS = [
+//   provideRouter(routes)
+// ];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
