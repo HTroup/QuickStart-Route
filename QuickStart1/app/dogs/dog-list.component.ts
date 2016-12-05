70d9ea193d9b5d5ffed6e44aec6acf9b2daaf757
Henry Troup <htroup@acm.org>
2016-12-04 20:20:12 -0500

    dog-list
    I had to subscribe to petfinder to get this working

--- QuickStart1/app/dogs/dog-list.component.ts
+++ QuickStart1/app/dogs/dog-list.component.ts
@@ -1,13 +1,43 @@
 ﻿// ====== ./app/Dogs/dog-list.component.ts ======
 
 // Import component decorator
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PetService } from '../pets.service'
+import 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
+//import { ROUTER_DIRECTIVES } from '@angular/router'; //deprecated
 
 @Component({
     template: `
     <h2>Dogs</h2>
-    <p>List of dogs</p>`
+    <p>List of dogs</p>
+    <ul class="demo-list-icon mdl-list">
+      <li class="mdl-list__item" *ngFor="let dog of dogs | async">
+        <span class="mdl-list__item-primary-content">
+            <i class="material-icons mdl-list__item-icon">pets</i>
+            <a [routerLink]="['/dogs', dog.id.$t]">{{ dog.name.$t }}</a>
+        </span>
+      </li>
+    </ul>
+    `
+     // Providers
+    // Already provided in the root module
+    //providers: [PetService]
 })
 
-// Component class
-export class DogListComponent { }
+// Component class implementing OnInit
+export class DogListComponent implements OnInit {
+
+    // Private property for binding
+    dogs: Observable<string[]>;
+
+    constructor(private petService: PetService) {
+
+    }
+
+    // Load data once component is ready
+    ngOnInit() {
+        // Pass retreived pets to the property
+        this.dogs = this.petService.findPets('dog');
+    }
+}
